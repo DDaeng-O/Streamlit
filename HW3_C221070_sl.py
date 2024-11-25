6f9074a25631541ccc7328d60e5ba91059c5e661
DDaeng-O <167838796+DDaeng-O@users.noreply.github.com>
2024-11-25 15:57:12 +0900

    Update HW3_C221070_sl.py

--- HW3_C221070_sl.py
+++ HW3_C221070_sl.py
@@ -30,7 +30,7 @@ st.divider()
 st.write('# 2. 데이터 정제하기')
 st.write('## (1) 합계출산율 데이터')
 '''
-# 컬럼 추출
+#### 컬럼 추출
 1. 합계출산율 열만 필요하므로, 인덱스가 되어줄 부분과 합계출산율 열만 가져온다. 
 2. 전국 데이터는 사용하지 않으므로 제거(drop)한다. 
 3. 행과 열의 이름이 너무 복잡하므로, 행렬의 이름을 더 직관적으로 수정한다.
@@ -46,27 +46,15 @@ st.divider()
 
 st.write('# 2. 두 데이터셋 간의 매칭 확인')
 '''
-# 데이터셋의 행정구역별 열에 대하여 매칭을 확인한다.
+#### 데이터셋의 행정구역별 열에 대하여 매칭을 확인한다.
 1. 먼저 각 데이터의 공백을 제거한다.
 2. 그후 두 데이터셋의 행정구역명에 대하여 서로 매칭되지 않는 값들이 있는지 확인한다.
 3. print 결과 아무 결과도 나오지 않으면 넘어가고, 그렇지 않은 경우 별도의 과정을 추가적으로 수행한다.
 '''
 st.write('## (1) 매칭 결과 확인')
 '''
-# '행정구역별'과 'CTP_KOR_NM'에서 공백 제거
-[코드]
-
-
-df_sel['행정구역별'] = df_sel['행정구역별'].str.strip()
-gdf_kor['CTP_KOR_NM'] = gdf_kor['CTP_KOR_NM'].str.strip()
-
-# 매칭되지 않은 데이터 확인
-unmatched = df_sel[~df_sel['행정구역별'].isin(gdf_kor['CTP_KOR_NM'])]
-print("매칭되지 않은 데이터:")
-print(unmatched)
-
-
- => 아무런 결과도 나오지 않는 것이 best!
+#### '행정구역별'과 'CTP_KOR_NM'에서 공백 제거
+##### => 아무런 결과도 나오지 않는 것이 best!
 '''
 df_sel['행정구역별'] = df_sel['행정구역별'].str.strip()
 gdf_kor['CTP_KOR_NM'] = gdf_kor['CTP_KOR_NM'].str.strip()
@@ -77,22 +65,7 @@ print(unmatched)
 
 st.write('## (2) 매칭되지 않는 데이터 처리 및 확인')
 '''
-- 만약 위 과정에서 결과가 나왔다면-
-[코드]
-
-
-
-name_corrections = {
-    '강원특별자치도': '강원도'
-}
-df_sel['행정구역별'] = df_sel['행정구역별'].replace(name_corrections)
-
-unmatched = df_sel[~df_sel['행정구역별'].isin(gdf_kor['CTP_KOR_NM'])]
-print("매칭되지 않은 데이터:")
-print(unmatched)
-
-
-
+- 만약 위 과정에서 결과가 나왔다면 위 결과에 기반하여 서로 매칭되지 않는 데이터들의 값을 임의로 조정한다.
 - 이 과정도 아무런 결과가 뜨지 않는 것이 best
 '''
 name_corrections = {
@@ -108,7 +81,7 @@ st.divider()
 
 st.write('# 3. 지도 시각화(folium)')
 '''
-# folium 라이브러리와 앞에서 정제한 데이터셋을 활용하여 지도로 시각화한다.
+##### folium 라이브러리와 앞에서 정제한 데이터셋을 활용하여 지도로 시각화한다.
 '''
 # Folium 지도 생성
 st.subheader("Choropleth 지도")
